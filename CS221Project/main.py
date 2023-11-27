4b813ab5b14a9562041af4f165cfdbe93ed0e1b3
Nina Boord <ninaboord@DN51vkjp.SUNet>
2023-11-26 23:58:55 -0800

    s

--- CS221Project/main.py
+++ CS221Project/main.py
@@ -4,25 +4,27 @@ import datetime as dt
 import sklearn 
 import torch
 
+
 def read(file):
     dataset = pd.read_csv('startup.data.csv')
     dtColumns = ['founded_at', 'closed_at', 'first_funding_at', 'last_funding_at']
+    columns_to_normalize = [col for col in dataset.columns if 'age' in col or 'funding' in col or 'milestone' in col or 'avg_participants' in col] + dtColumns
     dataset[dtColumns] = dataset[dtColumns].apply(lambda _: pd.to_datetime(_, format='%m/%d/%Y'))
     filtered_dataset = filter_unnamed(dataset)
-    columns_to_normalize = [col for col in filtered_dataset.columns if 'age' in col or 'funding' in col or 'milestone' in col or 'avg_participants' in col]
-    return filtered_dataset[columns_to_normalize]
+    filtered_dataset[columns_to_normalize] = normalize(filtered_dataset[columns_to_normalize])
     
-def median_centered(data_column):
-    return data_column - data_column.median()
+    def normalize(data_column):
+        return data_column - data_column.median()
 
-def filter_unnamed(dataframe):
-    columns_to_keep = [col for col in dataframe.columns if 'Unnamed' not in col] + dtColumns
-    return dataframe[columns_to_keep]
- 
+    def filter_unnamed(dataframe):
+        columns_to_keep = [col for col in dataframe.columns if 'Unnamed' not in col]
+        return dataframe[columns_to_keep]
+    
+    return filtered_dataset
 
 
 def main():
-    print("Hello World!")
+    print(read('startup.data.csv'))
 
 if __name__ == '__main__':
     main()
