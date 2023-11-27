a700d7d496673bd36a66d1e287c2b9be0e7e55d5
Hamed <hhekmat@stanford.edu>
2023-11-26 23:25:56 -0800

    mport statements and setting up main

--- CS221Project/main.py
+++ CS221Project/main.py
@@ -1,3 +1,14 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+import sklearn 
+import torch
+
+def read(file):
+    dataset = pd.read_csv('startup.data.csv')
+    def median
+ 
+
 def main():
     print("Hello World!")
 
