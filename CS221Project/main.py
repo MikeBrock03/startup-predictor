f2065ea0f3da23ad50188b4361bc94ab246a2f98
Hamed <hhekmat@stanford.edu>
2023-11-27 00:15:05 -0800

    preprocessing

--- CS221Project/main.py
+++ CS221Project/main.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import numpy as np
 import datetime as dt
@@ -6,12 +7,10 @@ import torch
 
 
 def read(file):
-    dataset = pd.read_csv('startup.data.csv')
+    dataset = pd.read_csv(file)
     dtColumns = ['founded_at', 'closed_at', 'first_funding_at', 'last_funding_at']
     columns_to_normalize = [col for col in dataset.columns if 'age' in col or 'funding' in col or 'milestone' in col or 'avg_participants' in col] + dtColumns
     dataset[dtColumns] = dataset[dtColumns].apply(lambda _: pd.to_datetime(_, format='%m/%d/%Y'))
-    filtered_dataset = filter_unnamed(dataset)
-    filtered_dataset[columns_to_normalize] = normalize(filtered_dataset[columns_to_normalize])
     
     def normalize(data_column):
         return data_column - data_column.median()
@@ -20,11 +19,17 @@ def read(file):
         columns_to_keep = [col for col in dataframe.columns if 'Unnamed' not in col]
         return dataframe[columns_to_keep]
     
+    filtered_dataset = filter_unnamed(dataset)
+    filtered_dataset[columns_to_normalize].apply(normalize)
+
+    
     return filtered_dataset
 
 
 def main():
-    print(read('startup.data.csv'))
+    p = os.path.join(os.path.dirname(__file__), "startup_data.csv")
+    print(read(p))
+
 
 if __name__ == '__main__':
     main()
