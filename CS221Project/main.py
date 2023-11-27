8eb8503f8270d658a81678f229d7d0ff8c6624ee
Nina Boord <ninaboord@DN51vkjp.SUNet>
2023-11-26 23:34:35 -0800

    filtering and medianizing

--- CS221Project/main.py
+++ CS221Project/main.py
@@ -6,8 +6,17 @@ import torch
 
 def read(file):
     dataset = pd.read_csv('startup.data.csv')
-    def median
+    filtered_dataset = filter_unnamed(dataset)
+    return filtered_dataset
+    
+def median_centered(data_column):
+    return data_column - data_column.mean()
+
+def filter_unnamed(dataframe):
+    columns_to_keep = [col for col in dataframe.columns if 'Unnamed' not in col]
+    return dataframe[columns_to_keep]
  
+
 
 def main():
     print("Hello World!")
