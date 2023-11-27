1b3d0178d01278eb784f60acadd27515aa4fc539
Hamed <hhekmat@stanford.edu>
2023-11-26 23:43:14 -0800

    dt preprocessing

--- CS221Project/main.py
+++ CS221Project/main.py
@@ -6,8 +6,17 @@ import torch
 
 def read(file):
     dataset = pd.read_csv('startup.data.csv')
-    def median
- 
+
+    # median centering columns with date values
+    dtColumns = ['founded_at', 'closed_at', 'first_funding_at', 'last_funding_at']
+    dataset[dtColumns] = dataset[dtColumns].apply(lambda _: pd.to_datetime(_, format='%m/%d/%Y'))
+    def medianCentering(column):
+        median = column.median
+        column = column - median
+    
+    columns_to_normalize +=  dtColumns
+    dataset[columns_to_normalize] = dataset[columns_to_normalize].apply(medianCentering(column))
+    
 
 def main():
     print("Hello World!")
