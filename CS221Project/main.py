749ce1605797a7589611e94a2bcf78f2016d5bce
Hamed <hhekmat@stanford.edu>
2023-12-01 14:54:01 -0800

    tuning hyperparam for NN

--- CS221Project/main.py
+++ CS221Project/main.py
@@ -2,15 +2,13 @@ import os
 import pandas as pd
 import numpy as np
 import datetime as dt
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, classification_report
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
-
-
 
 def read(file):
     dataset = pd.read_csv(file)
@@ -28,10 +26,8 @@ def read(file):
     
     filtered_dataset = filter_unnamed(dataset)
     filtered_dataset['first_funding_gap'] = filtered_dataset['first_funding_at'] - filtered_dataset['founded_at']
-    print(filtered_dataset.columns)
     filtered_dataset[columns_to_normalize] = filtered_dataset[columns_to_normalize].apply(normalize)
     filtered_dataset = filtered_dataset.applymap(lambda x: x.days if isinstance(x, pd.Timedelta) else x)
-    print(filtered_dataset.columns)
 
     columns_to_keep = columns_to_normalize + booleanColumns
     preprocessed_df = filtered_dataset[columns_to_keep]
@@ -41,7 +37,6 @@ def read(file):
 
     milestoneColumns = ['age_first_milestone_year','age_last_milestone_year']
     preprocessed_df[milestoneColumns] = preprocessed_df[milestoneColumns].fillna(1000000000000)
-    
 
     return preprocessed_df
 
@@ -56,7 +51,17 @@ def splitDataset(df):
 
 def logReg(X_train, y_train, X_val, y_val, X_test, y_test):
     model = LogisticRegression(random_state=42)
+    
     model.fit(X_train, y_train)
+
+    columns = X_train.columns
+
+    weights = model.coef_[0]
+
+    for column, weight in zip(columns, weights):
+        print(f"{column}: {weight}")
+
+    print(f"Intercept: {model.intercept_[0]}")
 
     y_val_pred = model.predict(X_val)
     accuracy = accuracy_score(y_val, y_val_pred)
@@ -67,7 +72,7 @@ def logReg(X_train, y_train, X_val, y_val, X_test, y_test):
     test_accuracy = accuracy_score(y_test, y_test_pred)
     print(f'Test Accuracy: {test_accuracy * 100:.2f}%')
 
-def prepare_data_NN(X_train, y_train, X_val, y_val, X_test, y_test, batch_size=32):
+def prepare_data_NN(X_train, y_train, X_val, y_val, X_test, y_test, batch_size):
     X_train_tensor = torch.tensor(X_train.values).float()
     y_train_tensor = torch.tensor(y_train.values).float()
 
@@ -88,6 +93,33 @@ def prepare_data_NN(X_train, y_train, X_val, y_val, X_test, y_test, batch_size=3
     test_loader = DataLoader(test_dataset, batch_size=batch_size)
 
     return train_loader, val_loader, test_loader
+
+def hyperparameter_tuning(train_loader, val_loader, learning_rates, batch_sizes, hidden_layer_configs, input_size):
+    best_val_accuracy = 0
+    best_hyperparams = {}
+
+    for lr in learning_rates:
+        for batch_size in batch_sizes:
+            for hidden_layers in hidden_layer_configs:
+                # Initialize model
+                model = BinaryClassifier(input_size, hidden_layers)
+                criterion = nn.BCELoss()
+                optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+                # Train model
+                trainNN(model, train_loader, val_loader, criterion, optimizer)
+
+                # Evaluate on validation set
+                val_loss, val_accuracy, _, _ = evalNN(model, val_loader, criterion)
+
+                print('curr lr', lr, 'curr batch size', batch_size, 'curr hidden', hidden_layers)
+
+                # Update best hyperparameters
+                if val_accuracy > best_val_accuracy:
+                    best_val_accuracy = val_accuracy
+                    best_hyperparams = (lr, batch_size, hidden_layers)
+
+    return best_hyperparams
 
 class BinaryClassifier(nn.Module):
     def __init__(self, input_size, hidden_layers, output_size=1):
@@ -175,13 +207,20 @@ def main():
     
     train_loader, val_loader, test_loader = prepare_data_NN(X_train, y_train, X_val, y_val, X_test, y_test, batch_size=32)
 
+    input_size = X_train.shape[1]
     # customize num hidden layers and nodes in them here!!
     hidden_layers = [5, 4, 4]
-    customNN = BinaryClassifier(X_train.shape[1], hidden_layers)
+    learning_rates = [0.1, 0.01, 0.001, 0.0001, .00001]
+    batch_sizes = [16, 32, 64, 128]
+    hidden_layer_configs = [[5], [5, 4], [64, 32], [5, 4, 4]]
+    best_lr, best_batch_size, best_hidden_layers = hyperparameter_tuning(train_loader, val_loader, learning_rates, batch_sizes, hidden_layer_configs, input_size)
+    print('best lr,', best_lr, 'best batch', best_batch_size, 'best hidden', best_hidden_layers)
+
+    train_loader, val_loader, test_loader = prepare_data_NN(X_train, y_train, X_val, y_val, X_test, y_test, best_batch_size)
+    customNN = BinaryClassifier(input_size, best_hidden_layers)
     
     criterion = nn.BCELoss()
-    optimizer = optim.Adam(customNN.parameters(), lr=0.001)
-
+    optimizer = optim.Adam(customNN.parameters(), best_lr)
 
     trainNN(customNN, train_loader, val_loader, criterion, optimizer, num_epochs=10)
     eval_loss = evalNN(customNN, test_loader, criterion)
